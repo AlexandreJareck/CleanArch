a051d5fd0951e95674b407559137bec6584db69d
AlexandreJareck <alexandrejareck@gmail.com>
2022-11-01 22:15:26 -0300

    feat: created irepositories

---
+++ src/CleanArch.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArch.Domain.Entities;
+
+namespace CleanArch.Domain.Interfaces;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProductsAsync();
+
+    Task<Product> GetByIdAsync(int? id);
+
+    Task<Product> GetProductCategoryAsync(int? id);
+
+    Task<Product> CreatedAsync(Product product);
+
+    Task<Product> UpdateAsync(Product product);
+
+    Task<Product> RemoveAsync(Product product);
+}
