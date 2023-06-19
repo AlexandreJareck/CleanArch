e802667fb7b901d64840ac10e6d3b337d73fb7b7
AlexandreJareck <alexandrejareck@gmail.com>
2023-06-19 14:33:09 -0300

    feat: created class concret service product

---
+++ src/CleanArch.Application/Services/ProductService.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+
+namespace CleanArch.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository ??
+                throw new ArgumentException(nameof(productRepository));
+
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var products = await _productRepository.GetProductsAsync();
+
+            return _mapper.Map<IEnumerable<ProductDTO>>(products);
+        }
+
+        public async Task<ProductDTO> GetProduct(int? id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task<ProductDTO> GetProductCategories(int? id)
+        {
+            var product = await _productRepository.GetProductCategoryAsync(id);
+
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task Add(ProductDTO productDTO)
+        {
+            var product = _mapper.Map<Product>(productDTO);
+
+            if (product != null)
+                await _productRepository.CreatedAsync(product);
+        }
+
+        public async Task Update(ProductDTO productDTO)
+        {
+            var product = _mapper.Map<Product>(productDTO);
+
+            if (product != null)
+                await _productRepository.UpdateAsync(product);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var product = _productRepository.GetByIdAsync(id).Result;
+            await _productRepository.RemoveAsync(product);
+        }
+    }
+}
