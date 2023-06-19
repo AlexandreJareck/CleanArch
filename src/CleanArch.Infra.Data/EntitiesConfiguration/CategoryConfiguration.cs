5c34b9eb16871224fab0c37368962891b7ca5393
AlexandreJareck <alexandrejareck@gmail.com>
2023-06-19 11:21:22 -0300

    feat: populate entite category in generate migrations

--- src/CleanArch.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
+++ src/CleanArch.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -10,6 +10,12 @@ namespace CleanArch.Infra.Data.EntitiesConfiguration
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios")
+            );
         }
     }
 }
