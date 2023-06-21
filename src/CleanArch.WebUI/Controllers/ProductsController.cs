2c4c7857cdf2794f1917fb97f3003f9cff1e2123
AlexandreJareck <alexandrejareck@gmail.com>
2023-06-21 19:19:53 -0300

    feat: created page for register product

--- src/CleanArch.WebUI/Controllers/ProductsController.cs
+++ src/CleanArch.WebUI/Controllers/ProductsController.cs
@@ -1,15 +1,19 @@
-﻿using CleanArch.Application.Interfaces;
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArch.WebUI.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
@@ -18,6 +22,26 @@ namespace CleanArch.WebUI.Controllers
             var products = await _productService.GetProducts();
 
             return View(products);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId =
+            new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
         }
     }
 }
